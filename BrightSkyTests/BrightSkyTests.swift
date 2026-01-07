64295cd04120bfd3f731e68ba6d3525f813210ef
Ritika <bhardwajritika11@gmail.com>
2026-01-08 04:06:58 +0530

    Initial Commit

---
+++ BrightSkyTests/BrightSkyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrightSkyTests.swift
+//  BrightSkyTests
+//
+//  Created by Tarun Sharma on 08/01/26.
+//
+
+import Testing
+@testable import BrightSky
+
+struct BrightSkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
